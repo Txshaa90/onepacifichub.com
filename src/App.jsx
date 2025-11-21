1c3a83887a4454e7b3f5fee99107d1680a8dd481
Trisha Caisip <trishacaisip1@gmail.com>
2025-11-21 14:45:01 +0800

    Remove Workspaces feature - not needed for e-commerce site

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import ProductsPage from './pages/ProductsPage'
 import AutoUpgradesPage from './pages/AutoUpgradesPage'
 import ProductDetailPage from './pages/ProductDetailPage'
 import CartPage from './pages/CartPage'
-import WorkspacesPage from './pages/WorkspacesPage'
-import WorkspaceDetailPage from './pages/WorkspaceDetailPage'
 import { CartProvider } from './context/CartContext'
 
 function App() {
@@ -22,8 +20,6 @@ function App() {
             <Route path="/products/:category" element={<ProductsPage />} />
             <Route path="/products/:category/:productId" element={<ProductDetailPage />} />
             <Route path="/cart" element={<CartPage />} />
-            <Route path="/workspaces" element={<WorkspacesPage />} />
-            <Route path="/workspaces/:id" element={<WorkspaceDetailPage />} />
           </Routes>
           <Footer />
         </div>
